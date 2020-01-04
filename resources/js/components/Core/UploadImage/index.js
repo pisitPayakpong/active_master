d6170891bd742bbf00db3e1213237d584bc60ba9
Pisit Payakpong <golfsert12@gmail.com>
2020-01-05 01:19:28 +0700

    merge

--- resources/js/components/Core/UploadImage/index.js
+++ resources/js/components/Core/UploadImage/index.js
@@ -23,16 +23,17 @@ class UploadImage extends Component {
             "image/gif",
             "image/jpg"
         ];
-        const isJpgOrPng = includes(typeImages);
-        if (!isJpgOrPng) {
+
+        if (!includes(typeImages, file.type)) {
             message.error("You can only upload JPG/PNG file!");
         }
+
         const isLt2M = file.size / 1024 / 1024 < 2;
         if (!isLt2M) {
             message.error("Image must smaller than 2MB!");
         }
 
-        return isJpgOrPng && isLt2M;
+        return false;
     };
 
     handleUpload = ({ file }) => {
